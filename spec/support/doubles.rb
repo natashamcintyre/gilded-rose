328dc130d80c9dc9539b934047499f7f877ad125
Natasha McIntyre <natashamc.cs@gmail.com>
2021-02-09 15:46:11 +0000

    tests written and passing

--- spec/support/doubles.rb
+++ spec/support/doubles.rb
@@ -7,6 +7,6 @@ RSpec.shared_context 'item_doubles', { shared_context: :metadata } do
   let(:brie) { Item.new('Aged Brie', SELL_IN, QUALITY) }
   let(:other) { Item.new('Other', SELL_IN, QUALITY) }
   let(:pass) { Item.new('Backstage passes to a TAFKAL80ETC concert', SELL_IN, QUALITY) }
-  let(:sulfuras) { Item.new('Sulfuras', SELL_IN, QUALITY) }
+  let(:sulfuras) { Item.new('Sulfuras, Hand of Ragnaros', SELL_IN, QUALITY) }
   let(:items) { [brie, other, pass, sulfuras] }
 end
--- spec/units/gildedrose_spec.rb
+++ spec/units/gildedrose_spec.rb
@@ -24,7 +24,7 @@ describe GildedRose do
     end
   end
 
-  context 'for aged brie' do
+  context 'for Aged Brie' do
     describe '#update_quality' do
       it 'increases quality by 1' do
         expect { subject.update_quality }.to change { brie.quality }.by(1)
@@ -36,9 +36,42 @@ describe GildedRose do
     end
   end
 
-  context 'for sulfuras' do
+  context 'for Sulfuras, Hand of Ragnaros' do
     describe '#update_quality' do
-      
+      it 'does not change quality' do
+        expect { subject.update_quality }.not_to change { sulfuras.quality }
+      end
+
+      it 'does not change sell in' do
+        expect { subject.update_quality }.not_to change { sulfuras.sell_in }
+      end
+    end
+  end
+
+  context 'for Backstage passes to a TAFKAL80ETC concert' do
+    describe '#update_quality' do
+      it 'decreases sell_in by 1' do
+        expect { subject.update_quality }.to change { pass.sell_in }.by(-1)
+      end
+
+      it 'increases quality by 1 (for over 10 days)' do
+        expect { subject.update_quality }.to change { pass.quality }.by(1)
+      end
+
+      it 'increases quality by 2 (for 5 to 10 days)' do
+        5.times { subject.update_quality }
+        expect { subject.update_quality }.to change { pass.quality }.by(2)
+      end
+
+      it 'increases quality by 3 (for less than 5 days)' do
+        10.times { subject.update_quality }
+        expect { subject.update_quality }.to change { pass.quality }.by(3)
+      end
+
+      it 'reduces quality to 0 when sell_in < 0' do
+        15.times { subject.update_quality }
+        expect { subject.update_quality }.to change { pass.quality }.to(0)
+      end
     end
   end
 end
